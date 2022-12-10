c909bfd871c85a1b6a241729c42aaf640fd0223e
ByLife <bylife@bylife.fr>
2022-12-10 19:07:52 +0100

    Added automatic routes ! :D Brain went brrrrr

--- src/routers/class.routers.ts
+++ src/routers/class.routers.ts
@@ -8,16 +8,26 @@ export class Routers {
         this.app = express()
     }
 
-    public getRoutesPath(route: any): any{
-        for(const [key, value] of Object.entries(route)){
-            if(key == "path") this.getRoutesPath(value)
-            else return value
-        }
+    iterate = (obj: any, path: string = "") => {
+        Object.keys(obj).forEach(key => {
+            if(key === "path") path += obj[key]
+
+            if (typeof obj[key] === 'object' && obj[key] !== null) {
+                    this.iterate(obj[key], path)
+            } else if (typeof obj[key] === 'function'){ 
+                if(path.includes("*")) this.app.get("*", obj[key])
+                else this.app.get(path, obj[key])
+            }
+        })
     }
 
     public initRoutes() {
-        console.log(this.getRoutesPath(Intercept));
-
+        var arr: Array<Function> = []
+        var path: string = ""
+        this.iterate(Intercept)
+        this.app.listen(3000, () => {
+            console.log("Server is running on port 3000")
+        })  
     }
 
 }
--- src/routers/response.routers.ts
+++ src/routers/response.routers.ts
@@ -4,36 +4,39 @@ import express from "express"
 
 export const Intercept = { // Intercept the requests and responses and route them to the right function
     path: "/api",
-    Errors : {
-        path: "/*",
-        E404: {
-            path: "",
-            res: (response: express.Response | null): void => {
-                response == null ? new Error("Unauthorized function manipulation") : 
-                response.json({
-                    status: "Error",
-                    message: "Not found"
-                })
-            }
-        }
-    },
     Users: {
         path: "/client",
         Connect: {
-            path:"/connect",
-            res: () : void => {
-                
+            path:"/connect/:publicAddress",
+            res: (req: express.Request, res: express.Response) : void => {
+                res.json({
+                    status: "success",
+                    message: `You are connect as ID: ${req.params.publicAddress}`
+                })
             }
         },
         Channel: {
-            path: "/channel",
+            path: "/channel/:token",
             res: (req: express.Request, res: express.Response): void => {
                 res.json({
                     status: "success",
                     message: `You are connect as ID: ${req.params.token}`
                 })
             }
-        }
+        },
+        Errors : {
+            path: "*",
+            E404: {
+                path: "",
+                res: (_: any, response: express.Response | null): void => {
+                    response == null ? new Error("Unauthorized function manipulation") : 
+                    response.json({
+                        status: "Error",
+                        message: "Not found"
+                    })
+                }
+            }
+        },
     }
 
 }
